b3760a9c1c4bf9855dc29121121d61775f0a1c03
DeepThought <collin.a.campbell-1@ou.edu>
2024-02-05 17:16:31 -0600

    wrote a stupid joke

--- mars.py
+++ mars.py
@@ -2,3 +2,9 @@
 
 # Read in data
 read_csv('mars_elements.csv')
+
+variable1 = input("what did the new coder say to the office?")
+if variable1=="Hello World":
+	print("REAL CODER!")
+	else:
+		print("imposter!")
